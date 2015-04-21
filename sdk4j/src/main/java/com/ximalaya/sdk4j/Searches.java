e287643f2c57febaf02096c7711d5573a3d5d4b2
will <will@ximalaya.com>
2015-04-21 11:25:35 +0800

    [updated]add category_id for search sdk

--- sdk4j/src/main/java/com/ximalaya/sdk4j/Searches.java
+++ sdk4j/src/main/java/com/ximalaya/sdk4j/Searches.java
@@ -18,45 +18,51 @@ public class Searches extends Ximalaya {
 	/**
 	 * 搜索专辑
 	 * 
-	 * @param q      搜索词，必填
+	 * @param q 搜索词，必填
+	 * @param categoryID 分类ID，0-热门分类，1-...
 	 * @param paging 分页参数，可选，不填则为默认值
 	 * @return
 	 * @throws XimalayaException
 	 */
-	public AlbumList searchAlbumList(String q, Paging paging) throws XimalayaException {
-		checkSearchParam(q, paging);
+	public AlbumList searchAlbumList(String q, long categoryID,  Paging paging) throws XimalayaException {
+		checkSearchParam(q, categoryID, paging);
 		return Album.constructAlbumList(
 				CLIENT.get(String.format("%s/search/albums", BASE_URL),
-						   assembleHttpParams(constructSpecificParamsForSearch(q, paging))));
+						   assembleHttpParams(constructSpecificParamsForSearch(q, categoryID, paging))));
 	}
 	
 	/**
 	 * 搜索声音
 	 * 
 	 * @param q      搜索词，必填
+	 * @param categoryID 分类ID，0-热门分类，1-...
 	 * @param paging 分页参数，可选，不填则为默认值
 	 * @return
 	 * @throws XimalayaException
 	 */
-	public TrackList searchTrackList(String q, Paging paging) throws XimalayaException {
-		checkSearchParam(q, paging);
+	public TrackList searchTrackList(String q, long categoryID, Paging paging) throws XimalayaException {
+		checkSearchParam(q, categoryID, paging);
 		return Track.constructTrackList(
 				CLIENT.get(String.format("%s/search/tracks", BASE_URL),
-						   assembleHttpParams(constructSpecificParamsForSearch(q, paging))));
+						   assembleHttpParams(constructSpecificParamsForSearch(q, categoryID, paging))));
 	}
 	
-	private void checkSearchParam(String q, Paging paging) {
+	private void checkSearchParam(String q, long categoryID, Paging paging) {
 		if(q == null || q.isEmpty()) {
 			throw new IllegalArgumentException("q should not empty");
+		}
+		if(categoryID < 0) {
+			throw new IllegalArgumentException("category_id should >= 0");
 		}
 		paging = (paging == null) ? new Paging() : paging;
 	}
 	
-	private HttpParameter[] constructSpecificParamsForSearch(String q, Paging paging) {
-		HttpParameter[] specificParams = new HttpParameter[3];
+	private HttpParameter[] constructSpecificParamsForSearch(String q, long categoryID,  Paging paging) {
+		HttpParameter[] specificParams = new HttpParameter[4];
 		specificParams[0] = new HttpParameter("q", q);
-		specificParams[1] = new HttpParameter("page", paging.getPage());
-		specificParams[2] = new HttpParameter("count", paging.getCount());
+		specificParams[1] = new HttpParameter("category_id", categoryID);
+		specificParams[2] = new HttpParameter("page", paging.getPage());
+		specificParams[3] = new HttpParameter("count", paging.getCount());
 		return specificParams;
 	}
 	
--- sdk4j/src/test/java/com/ximalaya/sdk4j/SearchesTest.java
+++ sdk4j/src/test/java/com/ximalaya/sdk4j/SearchesTest.java
@@ -13,13 +13,13 @@ public class SearchesTest {
 	
 	@Test
 	public void testSearchAlbum() throws XimalayaException {
-		AlbumList albumList = searchesService.searchAlbumList("郭德纲", new Paging());
+		AlbumList albumList = searchesService.searchAlbumList("郭德纲", 0, new Paging());
 		System.out.println(albumList);
 	}
 	
 	@Test
 	public void testSearchTracks() throws XimalayaException {
-		TrackList trackList = searchesService.searchTrackList("郭德纲", new Paging());
+		TrackList trackList = searchesService.searchTrackList("郭德纲", 0, new Paging());
 		System.out.println(trackList);
 	}
 
