04390d2aedcc5bcdfd43be03ae8d3bda271ea400
william <william.zhang@ximalaya.com>
2015-06-08 11:49:25 +0800

    [update]replace "get_playing _program" to "get_playing_program" william <william.zhang@ximalaya.com>

--- sdk4j/src/main/java/com/ximalaya/sdk4j/Lives.java
+++ sdk4j/src/main/java/com/ximalaya/sdk4j/Lives.java
@@ -104,7 +104,7 @@ public class Lives extends Ximalaya {
 	public Program getProgram(int radioID) throws XimalayaException {
 		checkRadioID(radioID);
 		return new Program(
-				CLIENT.get(String.format("%s/live/get_playing _program", BASE_URL), 
+				CLIENT.get(String.format("%s/live/get_playing_program", BASE_URL), 
 				            assembleHttpParams(new HttpParameter[] { new HttpParameter("radio_id", radioID) })));
 	}
 	
