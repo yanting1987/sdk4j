f2b899368a03de24127ef3579af284ec02ee6811
william10086 <williamzdp@gmail.com>
2015-07-20 18:12:06 +0800

    update test case

--- src/test/java/com/ximalaya/sdk4j/TracksTest.java
+++ src/test/java/com/ximalaya/sdk4j/TracksTest.java
@@ -14,27 +14,23 @@ public class TracksTest {
 
 	Tracks tracksService = new Tracks();
 
-//	@Test
-//	public void testGetHotTrackList() throws XimalayaException {
-//		TrackList trackList = tracksService.getHotTrackList(0, null, null);
-//		List<Track> tracks = trackList.getTracks();
-//		Assert.assertTrue(tracks != null && !tracks.isEmpty());
-//	}
+	@Test
+	public void testGetHotTrackList() throws XimalayaException {
+		TrackList trackList = tracksService.getHotTrackList(0, null, null);
+		List<Track> tracks = trackList.getTracks();
+		Assert.assertTrue(tracks != null && !tracks.isEmpty());
+	}
 
-/*	@Test
+	@Test
 	public void testBatchGetTracks() throws XimalayaException {
-		List<Track> tracks = tracksService.batchGetTracks(new long[] { 7368204,
-				7349613 }); // 有版权
+		List<Track> tracks = tracksService.batchGetTracks(new long[] { 7368204,7349613 }); // 有版权
 		Assert.assertTrue(tracks != null && tracks.size() > 0);
-	}*/
+	}
 
 	@Test
 	public void testGetLastPlayTracks() throws XimalayaException {
-		//TrackList trackList = tracksService.getLastPlayTracks(87415L, 20924L, 265031L, 5);
-		TrackList trackList = tracksService.getLastPlayTracks(86573L, 713L, 259713L, 2);
+		TrackList trackList = tracksService.getLastPlayTracks(12964151L, 275158L, 7861837L, 2);
 		List<Track> tracks = trackList.getTracks();
-		for(Track track:tracks){
-			System.out.println(track.getCoverUrlLarge());
-		}
+		Assert.assertTrue(trackList != null && tracks != null && tracks.size() > 0);
 	}
 }
