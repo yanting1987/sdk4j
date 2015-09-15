9a269ba14343a895f19f7f50adec2db1ece7fff2
wadson <[821286843@qq.com]>
2015-09-15 10:14:40 +0800

    UPDATE

--- src/test/java/com/ximalaya/sdk4j/LivesTest.java
+++ src/test/java/com/ximalaya/sdk4j/LivesTest.java
@@ -11,46 +11,46 @@ import com.ximalaya.sdk4j.model.Paging;
 import com.ximalaya.sdk4j.model.XimalayaException;
 
 public class LivesTest {
-	
-	Lives lives = new Lives();
-	
-	@Test
-	public void testGetProvinces() throws XimalayaException {
-		List<Province> provinces = lives.getProvinces();
-		Assert.assertTrue(provinces != null && !provinces.isEmpty());
-	}
-	
-	@Test
-	public void testGetRadioList() throws XimalayaException {
-		RadioList radioList = lives.getRadioList(1, "110000", new Paging());
-		Assert.assertTrue(radioList != null && radioList.getRadios() != null
-				&& !radioList.getRadios().isEmpty());
-	}
-	
-	@Test
-	public void testGetSchedules() throws XimalayaException {
-		List<Schedule> threeDaySchedules = lives.getSchedules(75);
-		Assert.assertTrue(threeDaySchedules != null && threeDaySchedules != null
-				&& !threeDaySchedules.isEmpty());
-	}
-	
-	@Test
-	public void testGetProgram() throws XimalayaException {
-		Program program = lives.getPlayingProgram(75);
-		Assert.assertNotNull(program != null && program.getProgramName() != null);
-	}
+
+    Lives lives = new Lives();
+
+    @Test
+    public void testGetProvinces() throws XimalayaException {
+        List<Province> provinces = lives.getProvinces();
+        Assert.assertTrue(provinces != null && !provinces.isEmpty());
+    }
+
+    @Test
+    public void testGetRadioList() throws XimalayaException {
+        RadioList radioList = lives.getRadioList(1, "110000", new Paging());
+        Assert.assertTrue(radioList != null && radioList.getRadios() != null
+                && !radioList.getRadios().isEmpty());
+    }
+
+    @Test
+    public void testGetSchedules() throws XimalayaException {
+        List<Schedule> threeDaySchedules = lives.getSchedules(75);
+        Assert.assertTrue(threeDaySchedules != null && threeDaySchedules != null
+                && !threeDaySchedules.isEmpty());
+    }
+
+    @Test
+    public void testGetProgram() throws XimalayaException {
+        Program program = lives.getPlayingProgram(75);
+        Assert.assertNotNull(program != null && program.getProgramName() != null);
+    }
 
 
-	@Test
-	public void testGetCitiesByProvince()throws XimalayaException{
-		List<City> cities=lives.getCityByProvince(320000);
-		Assert.assertNotNull(cities != null && cities.size() != 0);
-	}
+    @Test
+    public void testGetCitiesByProvince() throws XimalayaException {
+        List<City> cities = lives.getCityByProvince(320000);
+        Assert.assertNotNull(cities != null && cities.size() != 0);
+    }
 
-	@Test
-	public void testGetRadiosByCity() throws XimalayaException{
-	   RadioList radioList=lives.getRadiosByCity(3201, 1, 10);
-		System.out.println(radioList.getRadios().size());
-	}
+    @Test
+    public void testGetRadiosByCity() throws XimalayaException {
+        RadioList radioList = lives.getRadiosByCity(3201, 1, 10);
+        Assert.assertNotNull(radioList != null && radioList.getRadios().size() != 0);
+    }
 
 }
